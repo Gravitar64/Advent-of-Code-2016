26b3d0d08f778e27cedb46b845d6135d473deee5
Gravitar64 <gravitar@web.de>
2023-08-08 10:49:02 +0200

    numpy compact, 1,4ms

--- Tag_08.py
+++ Tag_08.py
@@ -1,41 +1,22 @@
 from time import perf_counter as pfc
 import re
-import itertools
-import collections
+import numpy as np
 
 
 def load(file):
   with open(file) as f:
-    return [x.strip() for x in f]
-
-
-def solve(p):
-  for zeile in p:
-    a, b = map(int, re.findall('\d+', zeile))
-    if zeile[:2] == 're':
-      rect(a, b)
-    else:
-      grid[a].rotate(b) if zeile[7] == 'r' else rotate_col(a,b)
-  return sum(sum(z) for z in grid)
-
-
-def rotate_col(col, n):
-  c = collections.deque([grid[y][col] for y in range(6)])
-  c.rotate(n)
-  for y, value in enumerate(c):
-    grid[y][col] = value
-
-
-def rect(a, b):
-  for x,y in itertools.product(range(a), range(b)):
-    grid[y][x] = 1
-
-
-puzzle = load('Tag_08.txt')
-width, height = 50,6
-grid = [collections.deque([0]*width) for _ in range(height)]
+    return f.readlines()
 
 start = pfc()
-print(solve(puzzle), pfc() - start)
+grid = np.zeros((6,50),dtype=int)
+puzzle = load('Tag_08.txt')
+
+for zeile in puzzle:
+  a, b = map(int, re.findall('\d+', zeile))
+  if zeile[:2] == 're': grid[:b, :a] = 1
+  elif zeile[7] == 'r': grid[a] = np.roll(grid[a],b)
+  else:                 grid[:,a] = np.roll(grid[:,a],b)
+    
+print(f'Part 1: {np.sum(grid)} in {pfc()-start} Sek.')
 for z in grid:
-  print(''.join(['*' if n == 1 else ' ' for n in z]))
+  print(''.join(['▓' if n == 1 else ' ' for n in z]))
