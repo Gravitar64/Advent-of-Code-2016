56669a0228c846d7f474e81c407b1e181cba6fed
Gravitar64 <gravitar@web.de>
2023-08-09 23:07:01 +0200

    Variablennamen angepasst

--- Tag_10.py
+++ Tag_10.py
@@ -10,27 +10,27 @@ def load(file):
 
 
 def solve():
-  output, bot, commands = defaultdict(list), defaultdict(list), {}
+  f = lambda x: map(int, re.findall('\d+', x))
+  outputs, bots, commands = defaultdict(list), defaultdict(list), {}
 
   for line in load('Tag_10.txt'):
     if line.startswith('value'):
-      chip, bot_to = map(int, re.findall('\d+', line))
-      bot[bot_to].append(chip)
+      chip, bot = f(line)
+      bots[bot].append(chip)
     else:
-      bot_from, dest1, dest2 = map(int, re.findall('\d+', line))
+      bot_from, dest1, dest2 = f(line)
       t1, t2 = re.findall(' (bot|output)', line)
       commands[bot_from] = (t1, dest1), (t2, dest2)
 
-  while bot:
-    for key, value in dict(bot).items():
-      if len(value) != 2: continue
-      v1, v2 = sorted(bot.pop(key))
-      (t1, dest1), (t2, dest2) = commands[key]
-      eval(f'{t1}[dest1].append(v1)')
-      eval(f'{t2}[dest2].append(v2)')
-      if v1 == 17 and v2 == 61: part1 = key
+  while bots:
+    for bot in [key for key,value in bots.items() if len(value) == 2]:
+      chip1, chip2 = sorted(bots.pop(bot))
+      (t1, dest1), (t2, dest2) = commands[bot]
+      eval(f'{t1}s[dest1].append(chip1)')
+      eval(f'{t2}s[dest2].append(chip2)')
+      if chip1 == 17 and chip2 == 61: part1 = bot
 
-  part2 = math.prod(output[i][0] for i in range(3))
+  part2 = math.prod(outputs[i][0] for i in range(3))
   return part1, part2
 
 
