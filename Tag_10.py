5fcb98c1f10bb88e3e82cd43ba17a675f47c4b7d
Gravitar64 <gravitar@web.de>
2023-08-09 22:43:59 +0200

    beautiful in 3,65ms

--- Tag_10.py
+++ Tag_10.py
@@ -6,47 +6,36 @@ import math
 
 def load(file):
   with open(file) as f:
-    return [(list(map(int,re.findall('\d+',line))), line.strip().split()) for line in f]
+    return [line.strip() for line in f]
 
 
 def solve():
-  outputs, bots, commands = {}, defaultdict(list), {}
-  bots_with_2 = [] 
-  
-  for numbers, command in load('Tag_10.txt'):
-    if len(numbers) == 2:
-      bots[numbers[1]].append(numbers[0])
-      if len(bots[numbers[1]]) == 2: bots_with_2.append(numbers[1])
+  output, bot, commands = defaultdict(list), defaultdict(list), {}
+
+  for line in load('Tag_10.txt'):
+    if line.startswith('value'):
+      chip, bot_to = map(int, re.findall('\d+', line))
+      bot[bot_to].append(chip)
     else:
-      commands[numbers[0]] = [(numbers[1], command[5]), (numbers[2], command[10])]
-      
-  while bots_with_2:
-    for key,value in bots.items():
-      if set((61,17)) == set(value): part1 = key
-    for bot_from in bots_with_2:
-      low_high = min(x for x in bots[bot_from]), max(x for x in bots[bot_from])
-      bots[bot_from] = []
-      for (bot_to, target), chip in zip(commands[bot_from], low_high):
-        if target == 'bot':
-          bots[bot_to].append(chip)
-        else:
-          outputs[bot_to] = chip
-    bots_with_2 = [key for key,value in bots.items() if len(value) == 2]
-  
-  part2 = math.prod(outputs[i] for i in range(3))
+      bot_from, dest1, dest2 = map(int, re.findall('\d+', line))
+      t1, t2 = re.findall(' (bot|output)', line)
+      commands[bot_from] = (t1, dest1), (t2, dest2)
+
+  while bot:
+    for key, value in dict(bot).items():
+      if len(value) != 2: continue
+      v1, v2 = sorted(bot.pop(key))
+      (t1, dest1), (t2, dest2) = commands[key]
+      eval(f'{t1}[dest1].append(v1)')
+      eval(f'{t2}[dest2].append(v2)')
+      if v1 == 17 and v2 == 61: part1 = key
+
+  part2 = math.prod(output[i][0] for i in range(3))
   return part1, part2
-    
-  
-            
-      
-      
-    
-      
-  
-      
- 
+
+
 start = pfc()
 part1, part2 = solve()
 print('Part 1:', part1)
-print('Part 2:', part2 )
+print('Part 2:', part2)
 print(pfc() - start)
