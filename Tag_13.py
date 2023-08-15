2fc92e92e64f6c6aa124f1bed69885c4b845e99f
Gravitar64 <gravitar@web.de>
2023-08-15 15:34:19 +0200

    V2, BFS 4,2ms

--- Tag_13.py
+++ Tag_13.py
@@ -1,4 +1,4 @@
-from time import perf_counter as pfc
+from time import perf_counter as t1
 
 
 def is_wall(x, y):
@@ -6,32 +6,24 @@ def is_wall(x, y):
   return bin(value).count('1') % 2
 
 
-def bfs():
-  seen = {(1, 1)}
-  steps = 0
-  new_locations = seen
+def bfs(start, end):
+  queue, seen = [(*start, 0)], set()
+  while True:
+    x, y, steps = queue.pop(0)
 
-  part1 = part2 = None
-  while not part1 or not part2:
-    old_locations = new_locations.copy()
-    new_locations = set()
-    
-    for x1,y1 in old_locations:
-      for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-        pos2 = x2,y2 = x1+dx, y1+dy
-        if x2 < 0 or y2 < 0 or is_wall(*pos2) or pos2 in seen: continue
-        seen.add(pos2)
-        new_locations.add(pos2)
-    
-    steps += 1
-    if (31, 39) in new_locations: part1 = steps
+    if (x, y) == end: return steps, part2
     if steps == 50: part2 = len(seen)
-  
-  return part1, part2  
 
-start = pfc()
-part1, part2 = bfs()
+    seen.add((x, y))
+    for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+      pos2 = x2, y2 = x+dx, y+dy
+      if x2 < 0 or y2 < 0 or is_wall(*pos2) or pos2 in seen: continue
+      queue.append((x2, y2, steps+1))
 
-print(f'Part1: {part1}')
-print(f'Part2: {part2}')
-print(f'Ermittelt in {pfc() - start:.5f} Sek.')
+
+start = t1()
+part1, part2 = bfs((1, 1), (31, 39))
+
+print(f'Part1: {part1} Steps')
+print(f'Part2: {part2} Locations')
+print(f'Ermittelt in {t1() - start:.5f} Sek.')
